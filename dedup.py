a7412dd8066945478099fd22e2192edff6e79cb9
David Chaloupka <chaloupka.david@gmail.com>
2016-11-03 18:25:45 +0100

    dedup - Making the decision, what to do with duplicates, interactive.

--- dedup.py
+++ dedup.py
@@ -1,16 +1,17 @@
 '''
 Author: David Chaloupka
-Date:   18.8.2010 - 22.7.2012
+Date:   18.8.2010 - 3.11.2016
 
 Description
-In the working directory deletes files that are bit duplicates. Matching is
-performed first by size and then by MD5 hash. Useful for deleting duplicate
-photos etc.
-
+Finds duplicate files in the working directory and its subdirectories and
+interactively decides what to do with them.
+Matching of duplicates is performed first by file size and then by MD5 hash.
 '''
 
 import os
 import os.path
+import sys
+import re
 import itertools
 import hashlib
 
@@ -78,48 +79,69 @@ class FileInfo:
             f2 and f2.close()
 
 
-VERBOSE = True # debug
+def listAllFiles(rootDir):
+    entries = [os.path.join(rootDir, e) for e in os.listdir(rootDir)]
+    files = [FileInfo(item) for item in entries if os.path.isfile(item)]
+    for subdir in [item for item in entries if os.path.isdir(item)]:
+        files.extend(listAllFiles(subdir))
+    return files
 
-# ziskani vsech souboru v adresari
-dirContent = os.listdir(".")
-files = [FileInfo(item) for item in dirContent if os.path.isfile(item)]
-files.sort(key=lambda x: x.getPath())
-# stable sort => zachova lexikograficke poradi predchoziho sortu
-# itertools.groupby vyzaduje, aby byl seznam serazen dle "groupovaciho klice"
-files.sort(key=lambda x: x.getSize())
+def groupsWithDuplicates(files):
+    # first group by file size...
+    # sort required by itertools.groupby
+    files.sort(key=lambda f: f.getSize())
+    equiSizeGroups = [list(group) for _, group in itertools.groupby(files, lambda f: f.getSize())]
+    equiSizeGroups = [g for g in equiSizeGroups if len(g) > 1]
 
-# shlukovani souboru podle velikosti
-groups = [list(group) for size, group in itertools.groupby(files, lambda f: f.getSize())]
-duplicateGroups = [group for group in groups if len(group) > 1]
-if VERBOSE:
-    print("possible duplicates by file size:")
-    if duplicateGroups:
-        for group in duplicateGroups:
-            print(" * " + ", ".join(map(lambda fileinfo: fileinfo.getPath(), group)))
+    # ...then group based on md5
+    duplicateGroups = []
+    for equiSizeGroup in equiSizeGroups:
+        equiSizeGroup.sort(key=lambda f: f.getMD5())
+        equiMD5Groups = [list(group) for _, group in itertools.groupby(equiSizeGroup, lambda f: f.getMD5())]
+        equiMD5Groups = [g for g in equiMD5Groups if len(g) > 1]
+        duplicateGroups.extend(equiMD5Groups)
+
+    return duplicateGroups
+
+def openFile(file):
+    if sys.platform == "linux":
+        os.system("xdg-open '%s' > /dev/null 2>&1" % file)
+    elif sys.platform == "win32":
+        os.system("open '%s'" % file)
+    elif sys.platform == "darwin":
+        os.system("start '%s'" % file)
     else:
-        print(" - none")
-    print()
+        raise Error("Unknown Operating system '%s'" % sys.platform)
 
-# detekce duplikatu testovanim na rovnost v O(n^2)
-# (slo by taky sesortit pomoci porovnani slozenym klicem (size, md5), kde
-# by se hash pocital pouze pro soubory stejne velikosti v __lt__, ale
-# souboru pravdepodobne nebude tolik, aby nas redukce na O(n*log n) trapila.
-for group in duplicateGroups:
-    while len(group) > 1:
-        originalFile = group.pop(0)
-        i = 0
-        while i < len(group):
-            iFile = group[i]
-            if originalFile == iFile: # dle velikosti a md5 hashe
-                try:
-                    os.remove(iFile.getPath())
-                    if VERBOSE:
-                        print("deleted file \"%s\"" % iFile.getPath())
-                except OSError as e:
-                    print("can't delete \"%s\" (%s)" % (iFile.getPath(), str(e)))
-                finally:
-                    # vymazeme duplikat (jinak soubor zustava do dalsi
-                    # iterace prvniho whilu)
-                    group.pop(i)
+if __name__ == "__main__":
+    files = listAllFiles(".")
+    groups = groupsWithDuplicates(files)
+    # show groups with biggest size first
+    groups.sort(key=lambda g: g[0].getSize(), reverse=True)
+
+    print("Found %d groups of identical files." % len(groups))
+
+    for group in groups:
+        resolved = False
+        while not resolved:
+            print()
+            print("Resolve following files:")
+            for i in range(len(group)):
+                print("  %d) %s" % (i+1, group[i]))
+            print()
+
+            action = input("Action? (Preserve #, Open, Next) ")
+            if action in ["n", "next"]:
+                resolved = True
+            elif action in ["o", "open"]:
+                openFile(os.path.normpath(group[i].getPath()))
+            elif re.match(r"p(preserve)? \d+", action):
+                iPreserve = int(re.match(r"p(preserve)? (\d+)", action).group(2)) - 1
+                for toDelete in [group[i] for i in range(len(group)) if i != iPreserve]:
+                    try:
+                        os.remove(toDelete.getPath())
+                    except OSError as e:
+                        print("Error: can't delete \"%s\" (%s)" % (toDelete.getPath(), str(e)))
+                resolved = True
             else:
-                i += 1
+                print("Unrecognized action '%s', try again" % action)
